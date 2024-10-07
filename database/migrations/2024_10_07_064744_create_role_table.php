a9750b800726cc9e233640c33eea9bdc65531dba
Marthin Pakpahan <marthinpakpahan@Marthins-MacBook-Pro.local>
2024-10-08 01:16:56 +0700

    initial commit

---
+++ database/migrations/2024_10_07_064744_create_role_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->text('description')->nullable();
+            $table->timestamp('created_at');
+            $table->timestamp('modified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role');
+    }
+};
